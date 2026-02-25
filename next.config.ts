bcf60c97da5ccec06c934075a3750a388a1214d4
Chandan <cgupta7510@gmail.com>
2026-02-25 13:03:47 +0530

    clerk webhoks enpoint created and tested in dev mode, working ok

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,20 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  allowedDevOrigins: [
+      "http://localhost:3000",
+      "http://127.0.0.1:3000",
+
+      // localtunnel
+      "https://*.loca.lt",
+
+      // ngrok
+      "https://*.ngrok.app",
+      "https://*.ngrok-free.app",
+
+      // Cloudflare Tunnel
+      "https://*.trycloudflare.com",
+    ],
 };
 
 export default nextConfig;
--- src/app/api/webhooks/clerk/register/route.ts
+++ src/app/api/webhooks/clerk/register/route.ts
@@ -1,5 +1,78 @@
+import { Webhook } from "svix";
+import { headers } from "next/headers";
+import { WebhookEvent } from "@clerk/nextjs/server";
+
+export async function POST(req: Request) {
+
+    const WEBHOOK_SECRET = process.env.WEBHOOK_SIGNING_SECRET;
+    if (!WEBHOOK_SECRET) {
+        throw new Error("Please add Web hook siging secret in .env")
+    }
+
+    const headerPlayload = headers();
+    const svix_id = (await headerPlayload).get("svix-id")
+    const svix_timestamp = (await headerPlayload).get("svix-timestamp")
+    const svix_signature = (await headerPlayload).get("svix-signature")
+
+    if (!svix_id || !svix_signature || !svix_timestamp) {
+        return new Error("Error occured, no svix headers.")
+    }
+
+    const payload = await req.json()
+    const body = JSON.stringify(payload)
+    const wh = new Webhook(WEBHOOK_SECRET);
+    let evt: WebhookEvent;
+
+    try {
+        evt = wh.verify(body, {
+            "svix-id": svix_id,
+            "svix-timestamp": svix_timestamp,
+            "svix-signature": svix_signature,
+        }) as WebhookEvent
+    } catch (err) {
+        console.error("Error verifying webhooks.", err)
+        return new Response("Error occures", { status: 400 })
+    }
+
+    // const { id } = evt.data
+    const eventType = evt.type
+
+    if (eventType === "user.created") {
+        try {
+            const { email_addresses, primary_email_address_id } = evt.data
+            // const primaryEmail = email_addresses.find((email) => email.id === primary_email_address_id)
+
+            // if (!primaryEmail) {
+            //     return new Response("No primary email found", { status: 400 })
+            // }
+
+            // Create a new user in DB
+            const newUser = {
+                id: evt.data.id,
+                email: email_addresses,
+            }
+            console.log("New user created", newUser)
+
+        } catch (error) {
+            console.log("Error creating user in DB", error);
+
+            return new Response("Error creating user in DB", { status: 400 })
+        }
+
+    }
 
 
-export async function POST(req:Request){
-    
+    if (eventType === "session.created") {
+        try {
+            const sessionData =  evt.data
+            console.log("Session created", sessionData)
+        } catch (error) {
+            console.log("Error saving session in DB", error);
+            return new Response("Error saving session in DB", { status: 400 })
+        }
+
+    }
+
+
+    return new Response("Webhook received successfully", { status: 200 })
 }
